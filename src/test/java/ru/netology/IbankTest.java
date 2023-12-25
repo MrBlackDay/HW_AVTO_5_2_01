10abd5d13df8d56c92e9204e85499f5edb7bbde5
Dima Lis <iselis777888999@gmail.com>
2023-12-25 23:18:37 +0300

    add test for unregiwtered user

--- src/test/java/ru/netology/IbankTest.java
+++ src/test/java/ru/netology/IbankTest.java
@@ -9,11 +9,12 @@ import java.time.Duration;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static ru.netology.DataGenerator.Registration.getRegisteredUser;
+import static ru.netology.DataGenerator.Registration.getUser;
 
 
 public class IbankTest {
     @Test
-    @DisplayName("Should successfully")
+    @DisplayName("Should successfully if login and password are registered")
     void shouldSuccessfully(){
         open("http://localhost:9999");
         var registeredUser = getRegisteredUser ("active");
@@ -21,7 +22,19 @@ public class IbankTest {
         $("[data-test-id='password'] input").setValue(registeredUser.getPassword());
         $("button.button").click();
         $("h2")
-                .shouldHave(Condition.exactText("Личный кабинет"), Duration.ofSeconds(5))
+                .shouldHave(Condition.exactText("Личный кабинет"), Duration.ofSeconds(10))
+                .shouldBe(Condition.visible);
+    }
+    @Test
+    @DisplayName("Should unsuccessfully if login and password are not registered")
+    void shouldUnsuccessfully(){
+        open("http://localhost:9999");
+        var notRegisteredUser = getUser ("active");
+        $("[data-test-id='login'] input").setValue(notRegisteredUser.getLogin());
+        $("[data-test-id='password'] input").setValue(notRegisteredUser.getPassword());
+        $("button.button").click();
+        $("[data-test-id= 'error-notification'] .notification__content")
+                .shouldHave(Condition.exactText("Ошибка! Неверно указан логин или пароль"), Duration.ofSeconds(10))
                 .shouldBe(Condition.visible);
     }
 }
